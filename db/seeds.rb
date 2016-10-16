164870b50b32a273246fdff34de548aade442aee
yanbin92 <ybinbin@outlook.com>
2016-10-16 17:13:27 +0800

    scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+# test data 
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+# . . .
+Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+description:
+%{<p>
+<em>Native Apps, Multiple Platforms</em>
+Answer the question “Can we build this for ALL the devices?” with a
+resounding YES. This book will help you get there with a real-world
+introduction to seven platforms, whether you’re new to mobile or an
+experienced developer needing to expand your options. Plus, you’ll find
+out which cross-platform solution makes the most sense for your needs.
+</p>},
+image_url: 'ruby.jpg',
+price: 26.00)
+# . . .
+Product.create(title: 'Agile Web Development with Rails',
+description:
+%{<p>
+<em>
+Each of the tutorial chapters in the fifth edition of Agile Web Development with Rails ends with a small set of additional exercises—things for readers to play with.
+</em>
+Use the pages below to discuss your solutions, problems you encountered, and so on.
+
+Enjoy exploring the various corners of Rails!
+</p>},
+image_url: 'rails.jpg',
+price: 186.00)
