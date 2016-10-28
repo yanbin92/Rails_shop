a9a0c8da1cf8a303df2cefe0640df1186df04c98
逸思邦 <Expand@Mac-mini.local>
2016-10-28 09:25:39 +0800

    add channel 类似观察者 需要修改配置文件 当数据库某个数据change

--- app/channels/products_channel.rb
+++ app/channels/products_channel.rb
@@ -2,6 +2,7 @@
 class ProductsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "products"
   end
 
   def unsubscribed
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,3 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+  config.action_cable.disable_request_forgery_protection = true
