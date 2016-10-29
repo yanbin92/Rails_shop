0d852517646a0340062a4b90c7b1f46477da3e0c
yanbin92 <ybinbin@outlook.com>
2016-10-29 22:11:49 +0800

    atom product.orders not return ???  todo fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 	has_many :line_items
+	has_many :orders,through: :line_items
 	before_destroy :ensure_not_referenced_by_any_line_ltem
 	validates(:title,:description,:image_url,:presence => true)
 	validates :price,:numericality=>{:greater_than_or_equal_to=>0.01}
