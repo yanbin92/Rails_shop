eb526cee1fece665b3ca8a99c8114ed3a59e5357
yanbin92 <ybinbin@outlook.com>
2017-02-10 15:57:51 +0800

    cookiestore

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_haitaoshop_session'
+#config/initializers/session_store.rb  如果想使用其他的会话存储机制，可以在 config/initializers/session_store.rb 文件中设
+#Rails 为 CookieStore 提供了一个密令，用来签署会话数据。这个密令可以在 config/secrets.yml 文件中修改：
+#使用 CookieStore 时，如果修改了密令，之前所有的会话都会失效。
--- config/routes.rb
+++ config/routes.rb
@@ -51,4 +51,6 @@ Rails.application.routes.draw do
   #get '/clients/:status' => 'clients#index', foo: 'bar'
 
   #在这个例子中，用户访问 /clients/active 时，params[:status] 的值是 "active"。同时，params[:foo] 的值也会被设为 "bar"，就像通过请求参数传入的一样。params[:action] 也是一样，其值为 "index"
+
+  
 end
