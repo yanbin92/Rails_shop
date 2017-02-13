5c941a0edc5300d3ce5f4a892140769b36688652
yanbin92 <ybinbin@outlook.com>
2017-02-13 13:51:24 +0800

    HTTP 摘要身份认证

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,17 @@ class AdminController < ApplicationController
   def index
   	@total_orders = Order.count
   end
+
+  #HTTP 摘要身份认证
+  #  USERS = { "lifo" => "world" }
+ 
+  # before_action :authenticate
+ 
+  # private
+ 
+  #   def authenticate
+  #     authenticate_or_request_with_http_digest do |username|
+  #       USERS[username]
+  #     end
+  #   end
 end
