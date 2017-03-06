1eea88f961aa39ba6d2141d59197b817e1265a47
yanbin92 <ybinbin@Outlook.com>
2017-03-06 18:02:50 +0800

    在邮件视图中添加图像

--- config/application.rb
+++ config/application.rb
@@ -14,15 +14,18 @@ module Haitaoshop
     config.autoload_paths += %w(#{Rails.root}/lib)
     config.active_record.schema_format = :sql
     # 一个应用的 :host 参数一般是不变的，可以在 config/application.rb 文件中做全局配置：
-    config.action_mailer.default_url_options = { host: 'example.com' }  
-    
-	# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-	# config.i18n.default_locale = :de
-	#ctive Job 为多种队列后端（Sidekiq、Resque、Delayed Job，等等）内置了适配器。最新的适配器列表参见 ActiveJob::QueueAdapters 的 API 文档。
-	# 要把适配器的 gem 写入 Gemfile
-	# 请参照适配器的具体安装和部署说明
-    # config.active_job.queue_adapter = :sidekiq
+    # config.action_mailer.default_url_options = { host: 'example.com' }  
+    #!!!!鉴于此，在邮件视图中不能使用任何 *_path 辅助方法，而要使用相应的 *_url 辅助方法
+    #在邮件视图中添加图像
+    config.action_mailer.asset_host = 'http://localhost:3000'
+
+  	# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+  	# config.i18n.default_locale = :de
+  	#ctive Job 为多种队列后端（Sidekiq、Resque、Delayed Job，等等）内置了适配器。最新的适配器列表参见 ActiveJob::QueueAdapters 的 API 文档。
+  	# 要把适配器的 gem 写入 Gemfile
+  	# 请参照适配器的具体安装和部署说明
+      # config.active_job.queue_adapter = :sidekiq    
     
   end
 end
