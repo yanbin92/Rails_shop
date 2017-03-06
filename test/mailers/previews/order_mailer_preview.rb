ab996e19d311ab11896530c8d70ed50da2316e37
yanbin92 <ybinbin@Outlook.com>
2017-03-06 16:35:41 +0800

    预览电子邮件

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
-
+  #预览电子邮件Action Mailer 提供了预览功能，通过一个特殊的 URL 访问。对上述示例来说，UserMailer 的预览类是 UserMailerPreview，存储在 test/mailers/previews/user_mailer_preview.rb 文件中。如果想预览 welcome_email，实现一个同名方法，在里面调用 UserMailer.welcome_email：
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/received
   def received
     OrderMailer.received
